18761a38ea3c1ad424138cf607a2374e5acade97
IvanovNR <IvanovNR@310D-12>
2024-12-05 09:12:59 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static string TrimString(string phrase, int len)
+        {
+            return phrase.Length <= len ? phrase : phrase.Substring(0, len - 3) + "...";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input_string = Console.ReadLine();
+            Console.WriteLine("Введите максимальную длину: ");
+            int maxLenght;
+            while (!int.TryParse(Console.ReadLine(), out maxLenght) || maxLenght < 1)
+            {
+                Console.WriteLine("Пожалуста, введите положительное число");
+            }
+
+            string trimPhrase = TrimString(input_string, maxLenght);
+            Console.WriteLine("Результат: " + trimPhrase);
+        }
+    }
+}
